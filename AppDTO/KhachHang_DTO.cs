d643b7e2c8c8783100c2e8179ecc6142eaeacc88
wMuoi50 <quockhanhp519@gmail.com>
2025-04-10 09:20:06 +0700

    Add file DTO

---
+++ AppDTO/KhachHang_DTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDTO
+{
+    public class KhachHang_DTO
+    {
+        public string MaKH {  get; set; }
+        public string TenKH {  get; set; }  
+        public string SDT{get; set; }
+        public string Email {  get; set; }
+        public string DiaChi {  get; set; }
+        public DateTime NgayTao { get; set; }
+        public int TrangThai {  get; set; }
+    }
+}
---
+++ AppDTO/SanPham_DTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDTO
+{
+    public class SanPham_DTO
+    {
+        public int MaSP {  get; set; }
+        public string TenSp { get; set; }
+        public string MaTacGia {  get; set; }
+        public string MaNXB {  get; set; }
+        public int MaLoai {  get; set; }
+        public double GiaNhap {  get; set; }
+        public double GiaBan {  get; set; }
+        public int SLTon {  get; set; }
+        public int TrangThai {  get; set; }
+    }
+}
